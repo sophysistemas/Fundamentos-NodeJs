9cac33028cec41eaeb5b155ea04007c7ddeb2dbf
sophysistemas <wellington@sophysistemas.com>
2020-05-20 10:49:41 -0300

    desafio concluído

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -14,15 +14,43 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const incomeSum = this.transactions.reduce((total, element) => {
+      if (element.type === 'income') {
+        let newTotal = total;
+        newTotal += element.value;
+        return newTotal;
+      }
+      return total;
+    }, 0);
+
+    const outcomeSum = this.transactions.reduce((total, element) => {
+      if (element.type === 'outcome') {
+        let newTotal = total;
+        newTotal += element.value;
+        return newTotal;
+      }
+      return total;
+    }, 0);
+
+    const balance = {
+      income: incomeSum,
+      outcome: outcomeSum,
+      total: incomeSum - outcomeSum,
+    };
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: Transaction): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,24 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ id, title, value, type }: Transaction): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+
+    const validBalance =
+      type === 'income' ? true : balance.income >= value + balance.outcome;
+
+    if (!validBalance) {
+      throw Error('This transaction goes beyond the companys cash');
+    }
+
+    const transaction = this.transactionsRepository.create({
+      id,
+      title,
+      value,
+      type,
+    });
+
+    return transaction;
   }
 }
 
